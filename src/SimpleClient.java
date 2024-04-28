1b5167dc3e364441d560f95a658deee72d349983
viharkheni <94455424+viharkheni@users.noreply.github.com>
2024-04-28 16:51:09 +0200

    final

--- src/SimpleClient.java
+++ src/SimpleClient.java
@@ -7,7 +7,7 @@ public class SimpleClient {
     private PrintWriter out;
     private BufferedReader in;
     private int portNumber = 5445;
-    static boolean ready = true;
+    //static boolean ready = true;
 
     public static void main(String[] args) {
         System.out.println("Simple Client Demo started....");
@@ -23,8 +23,8 @@ public class SimpleClient {
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             userInput = new BufferedReader(new InputStreamReader(System.in));
-while (true) {
 
+        while (true) {
             System.out.println("Enter your request (press Enter twice to send):");
             String line;
             StringBuilder requestBuilder = new StringBuilder();
@@ -32,27 +32,32 @@ while (true) {
             while ((line = userInput.readLine()) != null && !line.isEmpty()) {
                 requestBuilder.append(line).append("\n");
                 if (line.equals("exit")) {
-                    //System.out.println("Simple Client Demo finished!");
-                    //socket.close();
+                    System.out.println("Simple Client Demo finished!");
+                    socket.close();
                    break;
                 }
 
             }
-
             out.println(requestBuilder);
+            String response = null;
+            try {
+                response = in.readLine();
+            } catch (Exception e) {
 
-
-            String response = in.readLine();
-            System.out.println("Server response: " + response);
-
-            if (line.equals("exit")) {
-                System.out.println("Simple Client Demo finished!");
+            }
+            if(response == null) {
+                System.out.println("Connection closed by server");
                 break;
+            } else {
+                System.out.println("Server response: " + response);
+                if (response.equals("0 0")) {
+                    System.out.println("Simple Client Demo finished!");
+                    //socket.close();
+                    break;
+                }
             }
 
-
         }
-        socket.close();
         } catch (IOException e) {
             System.err.println("IOException during client operation: " + e.getMessage());
         }
